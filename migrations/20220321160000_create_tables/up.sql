dff97d7f499d3e47de13eb79306efd960ca18c0d
Alexander Ulmer <ulmer@student.tugraz.at>
2022-03-21 17:11:43 +0100

    basic sqlite database setup

---
+++ migrations/20220321160000_create_tables/up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR NOT NULL
+);
+
+INSERT INTO users (username) VALUES ("admin");
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
 #[macro_use] extern crate rocket;
-use rocket_sync_db_pools::{database, diesel};
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_migrations;
+use diesel::{Queryable, RunQueryDsl};
+use rocket_sync_db_pools::{database};
 
-use rocket::{routes, fs::NamedFile, response::status::NotFound, form::Form};
+mod schema {
+    use diesel::*;
+
+    table! {
+        users(id) {
+            id -> Integer,
+            username -> VarChar,
+        }
+    }
+}
+
+use schema::users;
+use rocket::{routes, fs::NamedFile, response::status::NotFound, form::Form, fairing::AdHoc, Rocket, Build};
+
+#[derive(Queryable)]
+struct User {
+    id: i32,
+    username: String,
+}
 
 #[get("/")]
-fn index() -> String {
-    format!("youre logged in as Bert")
+async fn index(conn: MainDatabase) -> String {
+    let mut usernames = String::new();
+    for user in conn.run(|c| {
+        users::dsl::users.load::<User>(c)
+    }).await.unwrap() {
+        usernames += &format!("{}, ", user.username);
+    }
+    usernames
 }
 
 #[get("/login")]
@@ -27,9 +54,19 @@ fn login(credentials: Form<Credentials>) -> String {
 #[database("main_db")]
 struct MainDatabase(diesel::SqliteConnection);
 
+async fn db_migrations(rocket: Rocket<Build>) -> Rocket<Build> {
+    embed_migrations!();
+
+    let conn = MainDatabase::get_one(&rocket).await.expect("database connection");
+    conn.run(|c| embedded_migrations::run(c)).await.expect("can run migrations");
+
+    rocket
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(MainDatabase::fairing())
+        .attach(AdHoc::on_ignite("DB Migrations", db_migrations))
         .mount("/", routes![index, serve_login, login])
 }
